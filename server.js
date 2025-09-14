83bdca7c3d9ea610ae6fe04f50bf1a0c44910c7d
omitoman-coder <omitoman@gmail.com>
2025-09-14 12:51:19 +0900

    修正版Ver2アップロード

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-// server.js
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(http);
+const io = require('socket.io')(http);
 
 app.use(express.static('public')); // public配信
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('新しいプレイヤー接続');
 
-  // 他のプレイヤーに手を送信
-  socket.on('move', (data) => {
+  // 他プレイヤーに手を送信
+  socket.on('move', data => {
     socket.broadcast.emit('move', data);
   });
 
@@ -20,6 +18,5 @@ io.on('connection', (socket) => {
   });
 });
 
-// Renderが割り当てるポート対応
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => console.log(`サーバー起動: http://localhost:${PORT}`));
